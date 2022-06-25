8d04f198f02367369e60ab1947749716feb79775
MarcosAM <marcosarruda.m@gmail.com>
2022-06-25 16:59:53 -0300

    feat: mo spawner

---
+++ Assets/MOSpawner.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MOSpawner : MonoBehaviour
+{
+    [SerializeField]
+    Transform[] spawnPoints;
+    float spawnRateBase = 0.4f;
+    float spawnRateVariation = 0.6f;
+
+    [SerializeField]
+    Projectile projectilePrefab;
+    [SerializeField]
+    Transform playersShip;
+
+    void Shoot(Vector3 origin)
+    {
+        Projectile projectile = Instantiate(projectilePrefab, origin, Quaternion.identity);
+        projectile.Fire((playersShip.position - origin).normalized);
+    }
+
+    void ShootFromRandomSpawnPoint()
+    {
+        int randomIdx = Random.Range(0, spawnPoints.Length);
+        Shoot(spawnPoints[randomIdx].position);
+    }
+
+    IEnumerator StartShooting()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRateBase + (Random.value * spawnRateVariation));
+            ShootFromRandomSpawnPoint();
+        }
+    }
+
+    void Start()
+    {
+        ShootFromRandomSpawnPoint();
+        StartCoroutine("StartShooting");
+    }
+}
