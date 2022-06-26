e9b97492060b1fa52f9a856a1aca711c350e16df
MarcosAM <marcosarruda.m@gmail.com>
2022-06-26 18:40:31 -0300

    fix: stabilizer push

--- Assets/Stabilizer.cs
+++ Assets/Stabilizer.cs
@@ -7,6 +7,11 @@ public class Stabilizer : ClickShipPart
     [SerializeField]
     float bonus;
 
+    void Awake()
+    {
+        rb2d = GetComponentInParent<Rigidbody2D>();
+    }
+
     public static float GetStabilizatonBonus(List<ShipPart> parts)
     {
         Stabilizer stabilizer = (Stabilizer)parts.Find(p => p is Stabilizer);
